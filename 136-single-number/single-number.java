1c29589adcefda972914630a9b7f076915e156de
Gobika <gobika0712@gmail.com>
2025-08-04 14:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (72.62%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    static{for(int i =0 ; i< 300 ;i++) singleNumber(new int[0]);}
+    public static int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int num : nums) ans ^= num;
+        return ans;
+    }
+}
