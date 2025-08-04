4539879bac10014f49fd8f2137857020b2713bcc
Gobika <gobika0712@gmail.com>
2025-08-04 14:29:48 +0530

    Time: 3 ms (37.83%) | Memory: 41.9 MB (47.43%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+         var stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                var temp = stack.pop();
+                if (s.charAt(i) == ')' && temp != '(' ||
+                        s.charAt(i) == '}' && temp != '{' ||
+                        s.charAt(i) == ']' && temp != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
