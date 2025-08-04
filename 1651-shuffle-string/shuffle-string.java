ca7445dbd0cd8cf9ff77feda9061dcb70aace48e
Gobika <gobika0712@gmail.com>
2025-08-04 14:53:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (41.61%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+         StringBuilder sb=new StringBuilder();
+        char res[]=new char[indices.length];
+        for(int i=0;i<s.length();i++){
+            res[indices[i]]=s.charAt(i);
+
+        }
+        return new String(res); 
+    }
+}
