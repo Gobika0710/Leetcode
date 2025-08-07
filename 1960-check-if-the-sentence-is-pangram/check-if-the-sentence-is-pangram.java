7997c7b344f174757e8cb31e025951775c331aaf
Gobika <gobika0712@gmail.com>
2025-08-07 18:21:27 +0530

    Time: 3 ms (42.76%) | Memory: 41.8 MB (29.50%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+    if(sentence.length()<26){
+        return false;
+    }   
+    Set<Character>set=new HashSet<>();
+    for(char ch:sentence.toCharArray()){
+        set.add(ch);
+    } 
+    return set.size()==26;
+    }
+}
