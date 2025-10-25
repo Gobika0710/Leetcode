4cab6cfbd365fef3b99007c27ab0453d04262726
Gobika <gobika0712@gmail.com>
2025-10-25 06:31:50 +0530

    Time: 0 ms (100%), Space: 40.9 MB (15.3%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalMoney(int n) {
+      
+        int weeks = n / 7;   
+        int days = n % 7;  
+        int total = 0;
+        for (int i = 0; i < weeks; i++) {
+            total += 28 + (i * 7);
+        }
+
+        int start = weeks + 1;
+        for (int i = 0; i < days; i++) {
+            total += start + i;
+        }
+
+        return total;
+    }
+}
+
